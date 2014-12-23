cc012068da292ab242d029294f427d1f61bf7820
minoritea <minorityland@gmail.com>
2014-12-23 11:11:45 +0900

    add render_element method

--- test/test_react_ruby.rb
+++ test/test_react_ruby.rb
@@ -3,139 +3,140 @@ require 'therubyracer'
 require 'react_ruby'
 require 'execjs'
 
-module JSXTestCase
-  def setup
-    set_runtime
-  end
+module TestCases
+  module JSXTestCase
+    def setup
+      set_runtime
+    end
 
-  def set_runtime
-    ExecJS.runtime = ExecJS::Runtimes::RubyRacer
-  end
-
-  def test_react_method
-    jsx = '<div>hello</div>'
-    assert_equal('React.createElement("div", null, "hello")',
-      ReactRuby::JSX.transform(jsx))
-  end
-
-  def test_react_method_with_multi_line_jsx
-    jsx = <<-JSX
-      this.Post = React.createClass({
-        render: function(){
-          return <div>
-            <Body message={"hello"}/>
-            Hello
-            </div>;
-        }
-      });
-    JSX
-    assert_equal(<<-JSX, ReactRuby::JSX.transform(jsx))
-      this.Post = React.createClass({displayName: 'Post',
-        render: function(){
-          return React.createElement("div", null, 
-            React.createElement(Body, {message: "hello"}), 
-            "Hello"
-            );
-        }
-      });
-    JSX
-  end
-end
-
-#defult runtime is therubyracer
-class JSXTest < Test::Unit::TestCase
-  include JSXTestCase
-end
-
-#if node.js is installed, do same test on node.js
-class JSXTestWithNode < Test::Unit::TestCase
-  if ExecJS::Runtimes::Node.available?
-    include JSXTestCase
     def set_runtime
-      ExecJS.runtime = ExecJS::Runtimes::Node
+      ExecJS.runtime = ExecJS::Runtimes::RubyRacer
+    end
+
+    def test_react_method
+      jsx = '<div>hello</div>'
+      assert_equal('React.createElement("div", null, "hello")',
+        ReactRuby::JSX.transform(jsx))
+    end
+
+    def test_react_method_with_multi_line_jsx
+      jsx = <<-JSX
+        this.Post = React.createClass({
+          render: function(){
+            return <div>
+              <Body message={"hello"}/>
+              Hello
+              </div>;
+          }
+        });
+      JSX
+      assert_equal(<<-JSX, ReactRuby::JSX.transform(jsx))
+        this.Post = React.createClass({displayName: 'Post',
+          render: function(){
+            return React.createElement("div", null, 
+              React.createElement(Body, {message: "hello"}), 
+              "Hello"
+              );
+          }
+        });
+      JSX
+    end
+  end
+
+  module RendererTestCase
+    def setup
+      set_runtime
+    end
+
+    def set_runtime
+      ExecJS.runtime = ExecJS::Runtimes::RubyRacer
+    end
+
+    def test_render_with_jsx
+      renderer = ReactRuby::Renderer.new(jsx: <<-JSX)
+         Test = React.createClass({
+           render: function(){
+             return <div>{this.props.testMessage}</div>;
+           }
+         });
+      JSX
+      assert_match(/<div [^>]+>test<\/div>/,
+        renderer.render('<Test testMessage="test"/>'))
+    end
+    
+    def test_render_with_js
+      renderer = ReactRuby::Renderer.new(js: <<-JS)
+         Test = React.createClass({displayName: 'Test',
+           render: function(){
+             return React.createElement("div", null, this.props.testMessage);
+           }
+         });
+      JS
+      assert_match(/<div [^>]+>test<\/div>/,
+        renderer.render('<Test testMessage="test"/>'))
+    end
+
+    def test_render_element
+      renderer = ReactRuby::Renderer.new(js: <<-JS)
+         Test = React.createClass({displayName: 'Test',
+           render: function(){
+             return React.createElement("div", null, this.props.testMessage);
+           }
+         });
+      JS
+      assert_match(/<div [^>]+>test<\/div>/,
+        renderer.render_element(:Test, testMessage:'test'))
+    end
+  end
+
+  module ReactRubyTestCase
+    def setup
+      set_runtime
+      ReactRuby.renderer = nil
+    end
+
+    def set_runtime
+      ExecJS.runtime = ExecJS::Runtimes::RubyRacer
+    end
+
+    def test_renderer
+      assert_nil(ReactRuby.renderer)
+      ReactRuby.compile
+      assert(ReactRuby.renderer)
+    end
+
+    def test_render_before_compile
+      assert_raise{ReactRuby.render('<div></div>')}
+      ReactRuby.compile
+      assert_nothing_thrown{ReactRuby.render('<div></div>')}
+    end
+  end
+
+  def self.create_tests
+    constants.each do |module_name|
+      next unless module_name =~ /\A(.+)TestCase\z/
+      base_name = $~[1]
+      m = const_get(module_name)
+
+      next unless m.is_a? Module
+
+      klass = Class.new(Test::Unit::TestCase) do
+        include m
+      end
+      const_set(base_name + 'Test', klass)
+
+      next unless ExecJS::Runtimes::Node.available?
+
+      klass_with_node = Class.new(Test::Unit::TestCase) do
+        include m
+        def set_runtime
+          ExecJS.runtime = ExecJS::Runtimes::Node
+        end
+      end
+
+      const_set(base_name + 'TestWithNode', klass_with_node)
     end
   end
 end
 
-module RendererTestCase
-  def setup
-    set_runtime
-  end
-
-  def set_runtime
-    ExecJS.runtime = ExecJS::Runtimes::RubyRacer
-  end
-
-  def test_render_with_jsx
-    renderer = ReactRuby::Renderer.new(jsx: <<-JSX)
-       Test = React.createClass({
-         render: function(){
-           return <div>{this.props.testMessage}</div>;
-         }
-       });
-    JSX
-    assert_match(/<div [^>]+>test<\/div>/,
-      renderer.render('<Test testMessage="test"/>'))
-  end
-  
-  def test_render_with_js
-    renderer = ReactRuby::Renderer.new(js: <<-JS)
-       Test = React.createClass({displayName: 'Test',
-         render: function(){
-           return React.createElement("div", null, this.props.testMessage);
-         }
-       });
-    JS
-    assert_match(/<div [^>]+>test<\/div>/,
-      renderer.render('<Test testMessage="test"/>'))
-  end
-end
-
-class RendererTest < Test::Unit::TestCase
-  include RendererTestCase
-end
-
-class RendererTestWithNode < Test::Unit::TestCase
-  if ExecJS::Runtimes::Node.available?
-    include RendererTestCase
-    def set_runtime
-      ExecJS.runtime = ExecJS::Runtimes::Node
-    end
-  end
-end
-
-module ReactRubyTestCase
-  def setup
-    set_runtime
-    ReactRuby.renderer = nil
-  end
-
-  def set_runtime
-    ExecJS.runtime = ExecJS::Runtimes::RubyRacer
-  end
-
-  def test_renderer
-    assert_nil(ReactRuby.renderer)
-    ReactRuby.compile
-    assert(ReactRuby.renderer)
-  end
-
-  def test_render_before_compile
-    assert_raise{ReactRuby.render('<div></div>')}
-    ReactRuby.compile
-    assert_nothing_thrown{ReactRuby.render('<div></div>')}
-  end
-end
-
-class ReactRubyTest < Test::Unit::TestCase
-  include ReactRubyTestCase
-end
-
-class ReactRubyTestWithNode < Test::Unit::TestCase
-  if ExecJS::Runtimes::Node.available?
-    include ReactRubyTestCase
-    def set_runtime
-      ExecJS.runtime = ExecJS::Runtimes::Node
-    end
-  end
-end
+TestCases.create_tests
